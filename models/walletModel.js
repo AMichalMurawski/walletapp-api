bcd06440b54deb192d30810205e2696254aca8ca
PiotrDrechsler <pdrech@gmail.com>
2023-04-28 12:03:22 +0200

    Update walletModel.js
    Added:
    - wallet model transaction

--- models/walletModel.js
+++ models/walletModel.js
@@ -3,9 +3,33 @@ const Schema = mongoose.Schema;
 
 const wallet = new Schema(
   {
-    // do uzupełnienia
+    date: {
+      type: String,
+      required: [true, 'date is required'],
+    },
+    type: {
+      type: String,
+    },
+    category: {
+      type: String,
+      required: [true, 'category is required'],
+    },
+    comment: {
+      type: String,
+    },
+    sum: {
+      type: String,
+      required: [true, 'sum is required'],
+    },
   },
-  { versionKey: false, timestamps: true }
+  {
+    versionKey: false,
+    timestamps: true,
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+    },
+  }
 );
 
 const Wallet = mongoose.model('wallet', wallet);
