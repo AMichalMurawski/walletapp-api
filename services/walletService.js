942b823fcab33882031cd1d9d84ddff85a771392
PiotrDrechsler <pdrech@gmail.com>
2023-05-02 20:39:19 +0200

    Update walletService.js
    refactor

--- services/walletService.js
+++ services/walletService.js
@@ -1,11 +1,11 @@
 const Wallet = require('../models/walletModel');
 
-const createWallet = ({ walletId, balance, transactions, owners }) =>
-  Wallet.create({
-    walletId,
-    balance,
-    transactions,
-    owners,
-  });
+const createWallet = ({ walletId, balance, transactions, owners }) => {
+  try {
+    return Wallet.create({ walletId, balance, transactions, owners });
+  } catch (err) {
+    return false;
+  }
+};
 
 module.exports = { createWallet };
