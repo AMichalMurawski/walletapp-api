e25abfc52a5905cbf1a2f581290b61c2e6245177
PiotrDrechsler <pdrech@gmail.com>
2023-05-02 19:48:26 +0200

    Update walletSchema.js
    changed for transation and wallet schema

--- schemas/walletSchema.js
+++ schemas/walletSchema.js
@@ -1,39 +1,33 @@
 const Joi = require('joi');
 
-const wallet = Joi.defaults(() =>
-  Joi.object({
-    date: Joi.date().required(),
-    type: Joi.string().valid('income', 'expense').required(),
-    category: Joi.string()
-      .valid(
-        'main expenses',
-        'products',
-        'car',
-        'self care',
-        'child care',
-        'household products',
-        'education',
-        'leisure',
-        'other expenses'
-      )
-      .required(),
-    comment: Joi.string(),
-    sum: Joi.number().min(1).required(),
-  })
-);
+const transactionSchema = Joi.object({
+  date: Joi.date().required(),
+  type: Joi.string().valid('income', 'expense').required(),
+  category: Joi.string()
+    .valid(
+      'main expenses',
+      'products',
+      'car',
+      'self care',
+      'child care',
+      'household products',
+      'education',
+      'leisure',
+      'other expenses'
+    )
+    .required(),
+  comment: Joi.string(),
+  sum: Joi.number().min(1).required(),
+});
 
-// walidacja dla dowolnego propsa (należy tutaj podać wszystkie)
-const atLeastOne = wallet
-  .object()
-  .or('date', 'type', 'category', 'comment', 'sum');
-
-// walidacja dla wymaganych wszystkich propsów
-const allRequired = wallet
-  .object()
-  .options({ presence: 'required' })
-  .required();
+const walletSchema = Joi.object({
+  walletId: Joi.string().required(),
+  balance: Joi.number().min(0).required(),
+  transactions: Joi.array().items(transactionSchema),
+  owners: Joi.array().items(Joi.string()),
+});
 
 module.exports = {
-  atLeastOne,
-  allRequired,
+  walletSchema,
+  transactionSchema,
 };
